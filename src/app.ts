203d1fe1aef37a409f33a3aa8d9c98f8289f1795
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-13 18:45:45 +0100

    Deleting connection to database for preserving YAGNI

--- src/app.ts
+++ src/app.ts
@@ -8,15 +8,6 @@ app.get('/', (_req, res) => {
   res.json('Hello world');
 });
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  next();
-});
-
 app.use(cors());
 app.use(express.json());
 app.use('/api/v1', apiRouter);
--- src/logger.test.ts
+++ src/logger.test.ts
@@ -2,7 +2,7 @@ import bunyan from 'bunyan';
 import logger from './logger.js';
 
 describe('Given a logger', () => {
-  test('Logger should be an instance of bunyan', async () => {
+  test('Logger should be an instance of bunyan', () => {
     expect(logger).toBeInstanceOf(bunyan);
   });
 });
