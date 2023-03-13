d3c9bd9c2ebb4f9f687b83fb4df73227e960b433
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-13 18:34:29 +0100

    Add api connection and cors to app

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
-export const hello = {
-  hello: 'Hello world',
-};
+import express from 'express';
+import apiRouter from './api/api-router.js';
+import cors from 'cors';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.json('Hello world');
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/v1', apiRouter);
+
+export default app;
