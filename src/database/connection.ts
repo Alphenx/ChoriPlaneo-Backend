7420656fc8a16d7dfe7562886a4d33c5c68c1436
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-14 14:13:33 +0100

    Reinstall mongoose and refactor connection to MongoDB

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,18 +1,5 @@
 import mongoose from 'mongoose';
-import log from '../logger.js';
 
-const connectDB = async (urlBD: string) => {
-  try {
-    mongoose.set('strictQuery', false);
-    mongoose.set('debug', true);
-
-    await mongoose.connect(urlBD);
-    log.debug('¡Database successfully connected!');
-    return true;
-  } catch (error) {
-    log.error('Error connecting to database');
-    throw new Error(`Error: ${error}`);
-  }
-};
+const connectDB = (urlBD: string) => mongoose.connect(urlBD);
 
 export default connectDB;
