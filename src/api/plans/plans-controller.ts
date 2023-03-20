f5a7dc928a01315385e76a650bc145411a415176
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-20 20:30:50 +0100

    Add plans controller, schema, router and tests

---
+++ src/api/plans/plans-controller.ts
@@ -0,0 +1,40 @@
+import { Locals, RequestHandler } from 'express';
+import { CustomHTTPError } from '../../utils/custom-http-error.js';
+import { queryProjectionPopulated } from '../users/users-types.js';
+
+import { Plan, PlanModel } from './plans-schema.js';
+import { queryProjectionPlan } from './plans-types.js';
+
+export const getPlansController: RequestHandler<unknown, Plan[]> = async (
+  _req,
+  res,
+  next,
+) => {
+  try {
+    const foundPlans = await PlanModel.find({}, queryProjectionPlan).exec();
+    res.json(foundPlans);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export interface PlanLocals extends Locals {
+  id: string;
+}
+
+export const getPlanByIdController: RequestHandler = async (req, res, next) => {
+  const { planId } = req.params;
+  try {
+    const plan = await PlanModel.findById({ _id: planId }, queryProjectionPlan)
+      .populate('registeredUsers', queryProjectionPopulated)
+      .exec();
+
+    if (plan === null) {
+      throw new CustomHTTPError(404, 'Plan not found.');
+    }
+
+    return res.json(plan);
+  } catch (error) {
+    next(error);
+  }
+};
