e077e3056cd7adb890f3fc9336950a7dcf766ce0
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-20 20:36:53 +0100

    Add user-types needed

---
+++ src/api/users/users-types.ts
@@ -0,0 +1,12 @@
+export interface UserLocalsAuthInfo {
+  email: string;
+}
+
+export const queryProjection = { __v: 0, password: 0 };
+export const queryProjectionPopulated = {
+  __v: 0,
+  password: 0,
+  name: 0,
+  email: 0,
+  profileURL: 0,
+};
