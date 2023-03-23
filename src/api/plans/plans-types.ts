f0c622618f6680a46076b3da755742fe34f303eb
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-23 15:39:47 +0100

    Add plans-schema and plans-types

--- src/api/plans/plans-types.ts
+++ src/api/plans/plans-types.ts
@@ -1,4 +1,16 @@
+import { Locals } from 'express';
 import { Plan } from './plans-schema.js';
 
 export const queryProjectionPlan = { __v: 0 };
 export type PlanRequest = Omit<Plan, 'planImgURL' | 'registeredUsers'>;
+
+export interface PlanLocals extends Locals {
+  id: string;
+}
+
+export const queryProjectionCreator = {
+  _id: 1,
+  name: 1,
+  email: 1,
+  profileURL: 1,
+};
