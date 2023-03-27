9691f031d3178155ccbd2837b8f6634fd2e039b1
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-27 10:13:11 +0200

    Remove required fields from plans-validation

--- src/api/plans/plans-validation.ts
+++ src/api/plans/plans-validation.ts
@@ -2,10 +2,10 @@ import { Joi } from 'express-validation';
 
 const plansValidation = {
   body: Joi.object({
-    title: Joi.string().min(3).max(50).required(),
-    description: Joi.string().min(3).max(200).required(),
-    place: Joi.string().min(3).max(50).required(),
-    date: Joi.date().required(),
+    title: Joi.string().min(3).max(50),
+    description: Joi.string().min(3).max(200),
+    place: Joi.string().min(3).max(50),
+    date: Joi.date(),
     status: Joi.string(),
   }),
 };
